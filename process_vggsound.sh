eb1117fbed282c857df68d3b1ca13e7ffac00a67
Daniil Zverev <zverev@in.tum.de>
2025-04-29 10:58:29 +0200

    Update process_vggsound.sh to modify dataset and output paths
    - Changed dataset path to include SLURM_ARRAY_TASK_ID for dynamic data handling.
    - Updated output CSV path to reflect modality-specific predictions.

--- process_vggsound.sh
+++ process_vggsound.sh
@@ -42,9 +42,9 @@ srun --exclusive --ntasks=1 python process_vggsound.py \
     --tokenizer_path config/llama2/tokenizer.model \
     --llama_config config/llama2/7B.json \
     --pretrained_path weights/consolidated.00-of-01.pth \
-    --dataset_path /tmp/vggsound \
+    --dataset_path /tmp/zverev/$SLURM_ARRAY_TASK_ID/vggsound \
     --video_csv ../../data/train.csv \
-    --output_csv ../../data/onellm_predictions.csv \
+    --output_csv csv/$modality/predictions.csv \
     --page $SLURM_ARRAY_TASK_ID \
     --per_page 1000 \
     --modality $modality \
